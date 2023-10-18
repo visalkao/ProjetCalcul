7c702a2998bfc862956883cf66ccf79568329d14
chadinel <chadinelhatmi@outlook.fr>
2023-10-18 15:19:17 +0100

    addded vbox pour en haut

--- src/View.java
+++ src/View.java
@@ -1,10 +1,17 @@
 
+
+
+import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
  
 public class View extends Application {
@@ -14,8 +21,19 @@ public class View extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
+        primaryStage.setTitle("Ma calculette");
+        VBox vbox = new VBox();
+        Text[] txt = new Text[5];
+        for (int i = 0; i < 5; i++) 
+        { 	txt[i]= new Text("0.00");
+        	StackPane s=new StackPane();
+        	Rectangle rectangle=new Rectangle(300,30);
+        	rectangle.setFill(Color.LIGHTGREY);
+            s.getChildren().addAll(rectangle,txt[i]); 
+            vbox.getChildren().add(s);
+            
+        } 
+       /* Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
@@ -23,11 +41,11 @@ public class View extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
-        });
+        });*/
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        //root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(vbox, 300, 250));
         primaryStage.show();
     }
 }
